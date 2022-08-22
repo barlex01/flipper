ecc68d2990d982fff10b0ab9ddb234d5ceb4207f
Alexei Rassudovskii <bar_lex@bk.ru>
2022-08-22 14:52:46 +0300

    addCustomGradient

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ $('.carousel').slick({
   dots: true
 })
 
-
+let customGradientLink = document.getElementById('customGradient');
 let simpleLink = document.getElementById('simple');
 let hexLink = document.getElementById('hex');
 let gradient = document.getElementById('gradient');
@@ -23,6 +23,15 @@ function setRandomRgb() {
   return `rgb(${getRandomPercent(255, 0)},${getRandomPercent(255, 0)},${getRandomPercent(255, 0)})`
 }
 
+  
+function onGradient() {
+  document.body.style.transition = `all 0.4s ease -in -out`;
+  document.body.style.background = `#ffffff`
+  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.background = `cover no - repeat; `
+  
+}
 function createElemDiv(color, cls, id) {
 
   let div = document.createElement('div');
@@ -58,12 +67,68 @@ function createElementForRenderGradient(clas){
                                 <a href="/" id ='radial'class="btn btn_radial">Radial-gradient</a>
                                 <div id='renderTwo'>
                                   <a href="/" id='circleAt' class='btn'>Circle at 100%</a>
+                                  
                                 </div>
                             </div>
                           </div>
                         `;
       render.append(div);
 }
+function createElementForCusomGradient(clas) {
+  let div = document.createElement('div');
+        div.innerHTML = `
+                          <div class="grid">
+                            <div class="left">
+                                
+                                <h3 class="flipper__title">Background: <span class="${clas}">white</span></h3>
+                            </div>
+                            <div class="center">
+                            </div>
+                            <div class="rigth">
+                                
+                            <div class="color">
+                            <input class="colors" type="color" id="color1" value="#00bfa5">
+                            <input class="colors" type="color" id="color2" value="#00332e">
+                        </div>
+                
+                        <div class="directions">
+                            <button value="down" class="active">
+                                <ion-icon name="arrow-down"></ion-icon>
+                            </button>
+                            <button value="up">
+                                <ion-icon name="arrow-up"></ion-icon>
+                            </button>
+                            <button value="left">
+                                <ion-icon name="arrow-forward"></ion-icon>
+                            </button>
+                            <button>
+                                <ion-icon name="arrow-back"></ion-icon>
+                            </button>
+                            <button class="rotate">
+                                <ion-icon name="arrow-down"></ion-icon>
+                            </button>
+                            <button class="rotate">
+                                <ion-icon name="arrow-up"></ion-icon>
+                            </button>
+                            <button class="rotate">
+                                <ion-icon name="arrow-forward"></ion-icon>
+                            </button>
+                            <button class="rotate">
+                                <ion-icon name="arrow-back"></ion-icon>
+                            </button>
+                        </div>
+                        <div class="result">
+                            <textarea class="code" ></textarea>
+                            <button class="copy">Copy</button>
+                        </div>
+                            </div>
+                          </div>
+                        `;
+      render.append(div);
+}
+
+
+
 
 simpleLink.addEventListener('click', simple)
 function simple(e) {
@@ -104,6 +169,9 @@ function simple(e) {
 //   }
 // }
 
+
+
+
 hexLink.addEventListener('click', hex)
 function hex(e) {
   // simpleLink.removeEventListener('click', simple)
@@ -137,6 +205,9 @@ function hex(e) {
   }
 }
 
+
+
+
 gradient.addEventListener('click', setGradient)
 
 function setGradient(e) {
@@ -146,19 +217,27 @@ function setGradient(e) {
   document.body.style.background = '#ffffff'
   
   createElementForRenderGradient('codeGradient');
-
+  
   let code = document.querySelector('.codeGradient');
   let linearBtn = document.getElementById('linear')
   let radialBtn = document.getElementById('radial')
   let circleAt = document.getElementById('circleAt')
+  let elipse = document.getElementById('ellipse');
+  
+  
   
   linearBtn.addEventListener('click',setLinearGradient);
-  radialBtn.addEventListener('click', setRadialGradient)
-  circleAt.addEventListener('click', setCircleGradient)
+  radialBtn.addEventListener('click', setRadialGradient);
+  circleAt.addEventListener('click', setCircleGradient);
+  
+  
   function setCircleGradient(e){
     e.preventDefault();
     onGradient()
-    code.textContent = document.body.style.background = `radial-gradient(circle at ${getRandomPercent(100, 0)}%, ${setRandomRgb()} ${getRandomPercent(36, 0)}%, ${setRandomRgb()} ${getRandomPercent(70, 36)}%, ${setRandomRgb()} ${getRandomPercent(100, 70)}%)`;
+   
+    code.textContent = document.body.style.background = `radial-gradient(circle at ${getRandomPercent(100, 0)}%,${setRandomRgb()} ${getRandomPercent(36, 0)}%, ${setRandomRgb()} ${getRandomPercent(70, 36)}%, ${setRandomRgb()} ${getRandomPercent(100, 70)}%)`;
+    console.log(code);
+
   }
   function setLinearGradient(e) {
 
@@ -166,7 +245,6 @@ function setGradient(e) {
     onGradient()
     
     code.textContent = document.body.style.background = `linear-gradient(${getRandomPercent(360,0)}deg, ${setRandomRgb()} ${getRandomPercent(36, 0)}%, ${setRandomRgb()} ${getRandomPercent(70, 36)}%, ${setRandomRgb()} ${getRandomPercent(100, 70)}%)`;
-    
   }
   
   function setRadialGradient(e) {
@@ -176,22 +254,30 @@ function setGradient(e) {
 
     code.textContent = document.body.style.background = `radial-gradient(circle, ${setRandomRgb()} ${getRandomPercent(36, 0)}%, ${setRandomRgb()} ${getRandomPercent(70, 36)}%, ${setRandomRgb()} ${getRandomPercent(100, 70)}%)`;
   }
-  
-function onGradient() {
-  document.body.style.transition = `all 0.4s ease -in -out`;
-  document.body.style.background = `#ffffff`
-  document.body.style.backgroundRepeat = 'no-repeat';
-  document.body.style.backgroundSize = 'cover';
-  document.body.style.background = `cover no - repeat; `
-  
+
 }
+customGradientLink.addEventListener('click', setCustomGradient)
+function setCustomGradient(e) {
+  e.preventDefault();
+  render.innerHTML = '';
+  document.body.style.background = `rgb(255,255,255)`;
+    onGradient();
+    createElementForCusomGradient('codeGradient');
+    let code = document.querySelector('.codeGradient')
+  let firstColor = document.getElementById('color1');  
+  let secondColor = document.getElementById('color2');  
+  firstColor.addEventListener('input', function(){
+    firstColor = this.value;
+      code.textContent = document.body.style.background = `linear-gradient(to bottom right, ${firstColor}, ${secondColor})`;
+    console.log(firstColor)
+    
+  })
+  secondColor.addEventListener('input', function() {
+    secondColor = this.value;
+    code.textContent = document.body.style.background = `linear-gradient(to bottom right, ${firstColor}, ${secondColor})`;
+  })
+  console.log(secondColor.value);
   
- 
   
-  
-
-
-
-
 
 }
